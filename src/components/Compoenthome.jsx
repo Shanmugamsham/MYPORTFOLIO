8477ebceb30e25c3533f518cb325d3bedc8c1563
Shanmugamsham <abdeshanmugam@gmail.com>
2024-07-22 20:50:26 +0700

    new

--- src/components/Compoenthome.jsx
+++ src/components/Compoenthome.jsx
@@ -7,6 +7,7 @@ import Componentabout2 from '../components2/Componentabout2';
 import Componentcontect2 from '../components2/Componentcontect2';
 const Compoenthome = () => {
   const [img,setimage]=useState("shan.pdf")
+  const [data,setdata]=useState("")
 
   function dowloading() {
     fetch(img).then((res)=>res.blob()).then((blobs)=>{
@@ -19,9 +20,93 @@ const Compoenthome = () => {
       document.body.removeChild(link)
     })
   }
-  setInterval(() => {
-    console.log("123");
+useEffect(()=>{
+  let MERN=document.getElementById("mern")
+  let FRONTEND=document.getElementById("fronend")
+  let REACT=document.getElementById("react")
+  let FULLSTACK=document.getElementById("fullstack")
+  setTimeout(() => {
+    let first=document.getElementById("hello")
+    first.innerText="Hello,I'm"
+  },500);
+
+  setTimeout(() => {
+    let first=document.getElementById("firstname")
+    first.innerText="S"
+  },1000);
+  setTimeout(() => {
+    let first=document.getElementById("sname")
+    first.innerText="H"
+  },1100);
+  setTimeout(() => {
+    let first=document.getElementById("tname")
+    first.innerText="A"
+  },1200);
+  setTimeout(() => {
+    let first=document.getElementById("foname")
+    first.innerText="N"
+  },1300);
+  setTimeout(() => {
+    let first=document.getElementById("fivename")
+    first.innerText="M"
+  },1400);
+  setTimeout(() => {
+    let first=document.getElementById("sixname")
+    first.innerText="U"
+  },1500);
+  setTimeout(() => {
+    let first=document.getElementById("sevenname")
+    first.innerText="G"
+  },1600);
+  setTimeout(() => {
+    let first=document.getElementById("eightname")
+    first.innerText="A"
+  },1700);
+  setTimeout(() => {
+    let first=document.getElementById("ninename")
+    first.innerText="S"
+  },1800);
+  setTimeout(() => {
+    let first=document.getElementById("tenname")
+    first.innerText="U"
+  },1900);
+  setTimeout(() => {
+    let first=document.getElementById("lvname")
+    first.innerText="D"
+  },2000);
+  setTimeout(() => {
+    let first=document.getElementById("twname")
+    first.innerText="A"
+  },2100);
+  setTimeout(() => {
+    let first=document.getElementById("thirname")
+    first.innerText="R"
+  },2200);
+  setTimeout(() => {
+    let first=document.getElementById("fivetname")
+    first.innerText="A"
+  },2300);
+  setTimeout(() => {
+    let first=document.getElementById("sixtname")
+    first.innerText="M"
+  },2400);
+  setTimeout(() => {
+    FULLSTACK.innerText="FULL STACK DEVELOPER"
+  },3000);
+  setTimeout(() => {
+    FULLSTACK.innerText=""
+    FRONTEND.innerText="FRONEND DEVELOPER"
   },5000);
+  setTimeout(() => {
+    FRONTEND.innerText=""
+    REACT.innerText="REACT DEVELOPER"
+  },7000);
+  setTimeout(() => {
+   REACT.innerText=""
+   MERN.innerText="MERN STACK DEVELOPER "
+  },9000);
+},[])
+
 
 
 
@@ -39,9 +124,34 @@ const Compoenthome = () => {
  </div>
 </div>
 <div className=' infodvi col-lg-6'>
-  <h5>Hello,I'm</h5>
-  <h1>SHANMUGA SUNDARAM</h1>
-  <h3>MERN STACK DEVELOPER</h3>
+  <h5>
+  Hello,I'm
+   </h5>
+  <h1><span id='firstname'></span>
+  <span id='sname'></span>
+  <span id='tname'></span>
+  <span id='foname'></span>
+  <span id='fivename'></span>
+  <span id='sixname'></span>
+  <span id='sevenname'></span>
+  <span id='eightname'></span>
+  <span id='ninename'></span>
+  <span id='tenname'></span>
+  <span id='lvname'></span>
+  <span id='twname'></span>
+  <span id='thirname'></span>
+  <span id='fourname'></span>
+  <span id='fivetname'></span>
+  <span id='sixtname'></span>
+  </h1>
+  <h3>
+    <span id='fullstack'></span> 
+  <span id='fronend'></span>
+  <span id='react'></span>
+  <span id='mern'></span>
+  
+  
+  </h3>
   <div className='buttondiv'>
   <button className='demobtns' onClick={dowloading}> Download CV</button>
   <button className='demobtns'> <Link to={"/contect"} className='demobtnslink' >Contect Info</Link></button>
